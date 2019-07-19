2e9b8a4bbd4bf23eba63a5d2c3503d4e1a594da1
LiliyaChuvachova <babak.liliya@mail.ru>
2019-07-19 19:14:24 +0300

    add next and prev

--- example1/jquery.js
+++ example1/jquery.js
@@ -1,22 +1,30 @@
-$(function() {
+$(function() { 
     //фкнкция вызова формы обратной связи
     $('#callback').click(function(){
         //появление окна обратной связи
         $('#grey').fadeIn();
-     
+        $('#close').fadeIn();
+        $('#grey').center();
+
         //выводим затемение страницы и делаем полупрозрачным
         $('#fade').css({'filter' : 'alpha(opacity=40)'}).fadeIn();
         return false;
     });
 
-   
     //функция закрытия окна
     $('#close, #fade').click(function() {
         $('#fade').fadeOut(function() {
-            $('#fade').remove();
-            $('#close').remove();
+            $('#fade').fadeOut();
+            $('#close').fadeOut();
             $('#grey').fadeOut();
         });
     });
  
 });
+
+jQuery.fn.center = function () {
+this.css("position","absolute");
+this.css("top", (($(window).height() - this.outerHeight()) / 2) + $(window).scrollTop() + "px");
+this.css("left", (($(window).width() - this.outerWidth()) / 2) + $(window).scrollLeft() + "px");
+return this;
+}
--- example1/test1.js
+++ example1/test1.js
@@ -1,21 +1,50 @@
-var MySlider = document.getElementsByClassName('cover');
-var currentPicture = 0;
-
-function stop(){//стоп анимации
-    for(var i=0; i<MySlider.length; i++){
-        MySlider[i].style.animation="none";
-    }
-}
-
-function show(){
-    MySlider[currentPicture].className = 'show';//z-index 3
-    MySlide[currentPicture-1].className = 'clear';//z-index 1  
-}
-
-function next(){
-    stop();
-    show();
-}
+var hwSlideSpeed = 1000;
+var hwTimeOut = 4000;
+var hwNeedLinks = true;
+ 
+$(document).ready(function() {
+    $('.slide').css(
+        {"position" : "absolute",
+         "top":'0', "left": '0'}).hide().eq(0).show();
+    var slideNum = 0;
+    var slideTime;
+    slideCount = $("#slider .slide").size();
+    var animSlide = function(arrow){
+        clearTimeout(slideTime);
+        $('.slide').eq(slideNum).fadeOut(hwSlideSpeed);
+        if(arrow == "next"){
+            if(slideNum == (slideCount-1)){slideNum=0;}
+            else{slideNum++}
+            }
+        else if(arrow == "prew")
+        {
+            if(slideNum == 0){slideNum=slideCount-1;}
+            else{slideNum-=1}
+        }
+        else{
+            slideNum = arrow;
+            }
+        $('.slide').eq(slideNum).fadeIn(hwSlideSpeed, rotator);}
+if(hwNeedLinks){
+var $linkArrow = $('<a id="prewbutton" href="#"></a><a id="nextbutton" href="#"></a>')
+    .prependTo('#slider');      
+    $('#nextbutton').click(function(){
+        animSlide("next");
+        })
+    $('#prewbutton').click(function(){
+        animSlide("prew");
+        })
+} 
+    var pause = false;
+    var rotator = function(){
+    if(!pause){slideTime = setTimeout(function(){animSlide('next')}, hwTimeOut);}
+            }
+    $('#slider-wrap').hover(    
+        function(){clearTimeout(slideTime); pause = true;},
+        function(){pause = false; rotator();
+        });
+    rotator();
+});
 
 
 
